248d41ff9a9835117fc1e10da0be3199da14c490
Parag <66076965+Parag18081994@users.noreply.github.com>
2023-10-24 03:34:06 +0000

    Data type summary

---
+++ 01_basic/datatypes-summary.js
@@ -0,0 +1,22 @@
+// Primitive 
+
+// 7 types : String , Number , Boolean , null , undefined , Symbol , BigInt
+
+
+// Reference type(Non primitive):  Array, Object , Function
+
+const heros=["Iron Man","Hanuman","Shaktiman"];
+let myobj={
+    name:"Parag",
+    age: 22,
+}
+
+
+const myFunction=function(){
+    console.log("Hello");
+}
+
+
+//to find datatype
+
+console.log(typeof myFunction);
